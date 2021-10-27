f49d6f5fa58638e88c7b01c7f8c429a1e6ce7c5c
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-10-27 16:47:25 +0900

    중간 데이터 구조 - 고객 데이터 이동
    고객 데이터를 중간 데이터로 옮김
    
    statementData 통해 전달되는 데이터를 모두 중간 데이터 구조로 옮기면, 계산 관련 코드는 전부 statement() 함수로 모으고 renderPlainText()는 data 매개변수로 전달된 데이터만 처리하게 만들 수 있습니다.

---
+++ 리팩터링 2판/사용 예시/Chapter01/24/Request.js
@@ -0,0 +1,72 @@
+function statement(invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;  // 고객 데이터를 중간 데이터로 옮김
+  return renderPlainText(statementData, invoice, plays);
+}
+
+function renderPlainText(data, invoice, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;  // 고객 데이터를 중간 데이터로 얻음
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(pref).name}: ${usd(amountFor(perf))} (${pref.audience}석)\n`;
+  }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(pref)
+    }
+    return volumeCredits;
+  }
+
+  function totalAmount() {
+    let reuslt = 0;
+    for (let pref of invoice.performances) {
+      reuslt += amountFor(perf);
+    }
+    return reuslt;
+  }
+}
