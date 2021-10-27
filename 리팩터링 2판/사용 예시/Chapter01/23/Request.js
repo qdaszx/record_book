51441c2736a311f050a6e06716c2963d30c5b878
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-10-27 16:39:02 +0900

    중간 데이터 구조를 인수로 전달
    리팩터링 단계 쪼개기로 두 단계로 나눠집니다.
    첫 번째 단계에서는 두 번째 단계로 전달할 중간 데이터 구조를 생성해야 하는데 그 역할을 하는 객체를 만들어서 인수로 전달합니다.

---
+++ 리팩터링 2판/사용 예시/Chapter01/23/Request.js
@@ -0,0 +1,71 @@
+function statement(invoice, plays) {
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays); // 중간 데이터 구조를 인수로 전달
+}
+
+function renderPlainText(data, invoice, plays) {   // 중간 데이터 구조를 인수로 전달
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(pref).name}: ${usd(amountFor(perf))} (${pref.audience}석)\n`;
+  }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(pref)
+    }
+    return volumeCredits;
+  }
+
+  function totalAmount() {
+    let reuslt = 0;
+    for (let pref of invoice.performances) {
+      reuslt += amountFor(perf);
+    }
+    return reuslt;
+  }
+}
